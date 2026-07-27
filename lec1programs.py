b7f5b3e27a828fbf4dca67c54f45c99a46e9f6cd
Shadhir Pasha <shadhirpashamohammed@gmail.com>
2026-07-27 23:40:19 +0530

    Add files via upload

---
+++ lec1programs.py
@@ -0,0 +1,26 @@
+#sum of 2 numbers
+# a = int(input("enter val1 :"))
+# b = int(input("enter val2 :"))
+# print(a+b)
+
+# area of a square
+# s = float(input("enter side of square :"))
+# print("area :",s*s)
+
+#avg of 2 floating values
+# a=float(input("enter val1 :"))
+# b=float(input("enter val2 :"))
+# print("average :",(a+b)/2)
+
+#boolean
+# a=float(input("enter a val1 :"))
+# b=float(input("enter a val2 :"))
+# print(a>=b)
+
+#boolean
+# a=float(input("enter a val1 :"))
+# b=float(input("enter a val2 :"))
+# if a>=b:
+#     print(True)
+# else:
+#     print(False)
