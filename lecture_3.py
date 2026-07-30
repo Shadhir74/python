e72e9168943cb1c2a21ccd306dbe1c77ce3de706
Shadhir Pasha <shadhirpashamohammed@gmail.com>
2026-07-30 23:10:54 +0530

    Add files via upload

---
+++ lecture_3.py
@@ -0,0 +1,12 @@
+#lists
+
+# marks=[55.2,22.5,95.5,66.3,69.3]
+# print(type(marks))
+# print(marks)
+# print(len(marks))
+# print(marks[3])
+
+student=["shadhir",22,55.5]
+print(student[0])                #mutable
+student[0]="ramu"
+print(student)
